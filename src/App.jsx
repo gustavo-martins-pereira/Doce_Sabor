c17a7251f329d3d801c79e931cddc184aef5c0e8
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-03-12 20:26:06 -0300

    ADD - Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
 import { Header } from "@/components/Header/Header";
+
+import "reset-css";
+import "normalize.css";
+
+const GlobalStyle = createGlobalStyle`
+    /*============================== Configurations ==============================*/
+    :root {
+        /*=============== COLORS ===============*/
+        --white-color: hsl(0, 0%, 100%);
+        --camelot-color: hsl(325, 45%, 32%);
+        --rouge-color: hsl(325, 46%, 46%);
+    
+        /*=============== BACKGROUNDS ===============*/
+        --header-linear-gradient: linear-gradient(to right, var(--rouge-color), var(--camelot-color));
+    
+        /*=============== BORDERS ===============*/
+    
+    
+        /*=============== TEXT COLORS ===============*/
+        --header-text-color: var(--white-color);
+    
+        /*=============== FONTS ===============*/
+        /*======= GENERIC =======*/
+        --ultra-font: "Ultra", "Arial", "Helvetica", serif;
+    }
+    
+    /*============================== GENERIC ELEMENTS ==============================*/
+    html,
+    body {
+        font-size: 16px;
+    }
+    
+    /*============================== GENERIC CLASSES ==============================*/
+    
+`;
 
 export function App() {
     return (
-        <>
+        <React.Fragment>
+            <GlobalStyle />
+
             <Header />
-        </>
+        </React.Fragment>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+    background-image: var(--header-linear-gradient);
+
+    padding: 1.5rem;
+
+    color: var(--header-text-color);
+    font-family: var(--ultra-font);
+    font-size: 2rem;
+    text-align: center;
+`;
+
 export function Header() {
     return (
-        <div>Testing Absolute Imports</div>
+        <HeaderStyled>Doce Sabor</HeaderStyled>
     );
 }
